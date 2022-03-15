fd9e00a9ee2aa7f210b095318eaee47d22863383
faiz-muhammed <fmuhammedfaiz99@gmail.com>
2022-03-15 18:15:12 +0530

    client changes

--- src/apiCalls/mainPageApi.js
+++ src/apiCalls/mainPageApi.js
@@ -1,17 +1,22 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/api";
+const BASE_URL = "http://184.72.69.144/api";
+// const BASE_URL = "http://localhost:3001/api";
 
 const api = {
-  quoteChange: (color,quote) => {
+  quoteChange: (color, quote) => {
     return new Promise((resolve, reject) => {
-      let data={changedColor:color,Quote:quote.input}
-      axios.post(`${BASE_URL}/quoteChange`, data).then((res) => {
-        resolve(res);
-      }).catch((err) => {console.log("quote api call error")});
+      let data = { changedColor: color, Quote: quote.input };
+      axios
+        .post(`${BASE_URL}/quoteChange`, data)
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((err) => {
+          console.log("quote api call error");
+        });
     });
   },
-
 };
 
 export default api;
